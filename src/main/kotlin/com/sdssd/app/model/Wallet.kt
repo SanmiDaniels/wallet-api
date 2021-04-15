9eafba2d44e8427616b9ae53778f9d9296d46964
SanmiDaniels <oluwasanmiadedaniel@gmail.com>
2021-04-15 01:24:45 +0100

    Documented wallet........

--- src/main/kotlin/com/sdssd/app/model/Wallet.kt
+++ src/main/kotlin/com/sdssd/app/model/Wallet.kt
@@ -1,5 +1,6 @@
 package com.sdssd.app.model
 import com.fasterxml.jackson.annotation.JsonIgnore
+import io.swagger.v3.oas.annotations.media.Schema
 import org.hibernate.annotations.GenericGenerator
 import java.math.BigDecimal
 import java.math.BigInteger
@@ -12,20 +13,27 @@ class Wallet {
         @Id
         @GeneratedValue(strategy = GenerationType.AUTO)
         @Column(columnDefinition = "BINARY(16)")
+        @Schema( description = "Unique ID of wallet")
         var id: UUID? = null
 
         @ManyToOne
         @JsonIgnore
         @JoinColumn(name = "user_email")
+        @Schema(description = "User that owns the wallet")
         var user: User? = null
+        @Schema(description = "Currency the wallet holds")
         var currency: Currency? = null
+        @Schema(description = "Balance of the wallet")
         var balance: BigDecimal? = BigDecimal.valueOf(0)
+        @Schema(description = "Shows if wallet is user's main or not")
         var isMain = false
 
         @OneToMany(cascade = [CascadeType.ALL], mappedBy = "toWallet",fetch = FetchType.LAZY)
+        @Schema(description = "All transactions carried out to the wallet")
         var toTransactions: MutableSet<Transaction> = HashSet()
 
         @OneToMany(cascade = [CascadeType.ALL], mappedBy = "fromWallet",fetch = FetchType.LAZY)
+        @Schema(description = "All transactions carried out from the wallet")
         var fromTransactions: MutableSet<Transaction> = HashSet()
 
         constructor() {}
